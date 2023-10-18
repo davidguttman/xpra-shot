79b58450fb383d2f71c2b82e8302517218c33e28
David Guttman <david@davidguttman.com>
2023-10-17 17:48:13 -0700

    use xvfb?

--- server.js
+++ server.js
@@ -6,22 +6,20 @@ const { exec } = require('child_process');
 const crypto = require('crypto');
 const fs = require('fs');
 
-// Start xpra on server startup to create a display
-exec('xpra start :99', (error, stdout, stderr) => {
+// Start xvfb on server startup to create a display
+exec('Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &', (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
   }
-  console.log(stdout);
-  console.error(stderr);
-  console.log('Xpra started successfully');
+  console.log('Xvfb started successfully');
 });
 
 app.get('/xpra', (req, res) => {
   const host = req.query.host;
   const password = req.query.password;
   const random = crypto.randomBytes(20).toString('hex');
-  const attachCommand = `DISPLAY=:99 XPRA_PASSWORD=${password} xpra attach --speaker=disabled ws://${host}`;
+  const attachCommand = `DISPLAY=:99 XPRA_PASSWORD=${password} xpra attach ws://${host}`;
   const screenshotCommand = `XPRA_PASSWORD=${password} xpra screenshot ${random}.jpg ws://${host}`;
   const detachCommand = `XPRA_PASSWORD=${password} xpra detach ws://${host}`;
   
