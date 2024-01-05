78162705827819ba5bcd23dc8d382b79d7691cc0
David Guttman <david@davidguttman.com>
2024-01-05 09:27:25 -0800

    fix: prevent partial transfer, maybe

--- server.js
+++ server.js
@@ -11,35 +11,50 @@ const port = process.env.PORT || 3000
 const CROP_X = 5
 const CROP_Y = 31
 
-app.get('/xpra', (req, res) => {
+app.get('/xpra', async (req, res) => {
   const host = req.query.host
   const password = req.query.password
   const random = crypto.randomBytes(20).toString('hex')
   const command = `XPRA_PASSWORD=${password} xpra screenshot ${random}.jpg ws://${host}`
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) return handleError(res, `exec error: ${error}`)
-
+  try {
+    await executeCommand(command)
     const filePath = path.resolve(__dirname, `${random}.jpg`)
-    Jimp.read(filePath)
-      .then(image => {
-        const w = image.bitmap.width - CROP_X
-        const h = image.bitmap.height - CROP_Y
-        return image.crop(CROP_X, CROP_Y, w, h)
-      })
-      .then(image => image.write(filePath))
-      .then(() => {
-        res.sendFile(filePath, err => {
-          if (err) return handleError(res, `sendFile error: ${err}`)
-
-          fs.unlink(filePath, err => {
-            if (err) handleError(res, `unlink error: ${err}`)
-          })
-        })
-      })
-      .catch(err => handleError(res, err))
-  })
+    await cropImage(filePath)
+    await sendFile(res, filePath)
+    console.log(`File ${filePath} sent`)
+  } catch (error) {
+    handleError(res, error)
+  }
 })
+
+const executeCommand = command => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) reject(`exec error: ${error}`)
+      resolve()
+    })
+  })
+}
+
+const cropImage = filePath => {
+  return Jimp.read(filePath)
+    .then(image => {
+      const w = image.bitmap.width - CROP_X
+      const h = image.bitmap.height - CROP_Y
+      return image.crop(CROP_X, CROP_Y, w, h)
+    })
+    .then(image => image.write(filePath))
+}
+
+const sendFile = (res, filePath) => {
+  return new Promise((resolve, reject) => {
+    res.sendFile(filePath, err => {
+      if (err) reject(`sendFile error: ${err}`)
+      resolve()
+    })
+  })
+}
 
 const handleError = (res, error) => {
   console.error(error)
